e9da5fdf84a0dffa0608925a07e66711024f3c56
KOUSTAV DAS <koustav.das213@gmailcom>
2018-04-14 00:25:50 +0530

    Done

--- train_script.m
+++ train_script.m
@@ -1,16 +1,14 @@
 train=zeros(0,0);
-for i=1:20
+for i=1:19
     filename=['Positive/' num2str(i) '.wav'];
     train=[train;Feature_Extraction(filename,1)];
 end
-%%
-for i=1:9
+for i=1:2
 filename= ['Negative/' num2str(i) '.wav'];
 train=[train;Feature_Extraction(filename,0)];
 end
-%%
 n=size(train,2);
-Mdl=fitcknn(train(:,1:n-1),train(:,n));
+Mdl=fitcsvm(train(:,1:n-1),train(:,n));
 
 %%
 for i=1:1
